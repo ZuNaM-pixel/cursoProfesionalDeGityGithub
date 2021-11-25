b79ed9c9303acdfa3b996d012406a465a7e11232
zunam <quenallatafranco@gmail.com>
2021-11-25 15:13:32 -0300

    Nos mudamos a react js

--- test/src/App.js
+++ test/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Combinamos git y reactjs.
         </a>
       </header>
     </div>
